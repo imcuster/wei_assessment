5b237340d4ed81c965bb5532901ca386d13b5d6b
imcuster <imcuster@gmail.com>
2020-05-06 17:16:23 -0400

    Making some final changes before walking away for a bit.

--- python_query/ec2_query.py
+++ python_query/ec2_query.py
@@ -1,8 +1,6 @@
 #!/usr/local/bin/python3
 
 import boto3
-from botocore.exceptions import ClientError
-from openpyxl import Workbook
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -12,46 +10,34 @@ args = parser.parse_args()
 
 session = boto3.session.Session(profile_name=args.profile)
 regions = session.get_available_regions('ec2')
-#region_name = args.region
 
 def list_instances():
-    #for region in regions:
     ec2 = session.client(service_name='ec2', region_name=region)
     instances = ec2.describe_instances()
     disk_info = ec2.describe_volumes()
+    volume_size = disk_info['Volumes']
     for instance in instances['Reservations']:
         instance_dict = instance['Instances']
         for item in instance_dict:
+            # This script can fail if it runs into a terminated instance.
+            if item['State']['Name'] == 'terminated':
+                break
             # Get a list of attached volumes to user later
             volume_info = item['BlockDeviceMappings']
             # Get the instance ID
             print("Instance ID:", item['InstanceId'])
             # Get the VPC it lives in
+            # It appears this fails if there's a terminated instance in your list?
             print("VPC:", item['VpcId'])
             # Get the number of attached disks
             print("Number of attached disks:", len(volume_info))
             # Get information about attached volumes
             for volume in volume_info:
                 volume_id = volume['Ebs']['VolumeId']
-                #print(volume['Ebs']['VolumeId'])
                 print("Volume ID:", volume_id)
                 print("Volume mount point:", volume['DeviceName'])
-
-    # for region in regions:
-    #     ec2 = session.client(service_name='ec2', region_name=region)
-    #     response = ec2.describe_instances()
-    #     for instance in response:
-    #         print(instance)
-        # try:
-        #     responses = ec2.describe_instances()
-        # except ClientError as e:
-        #     if e.response['Error']['Code'] == 'NoCredentialsError':
-        #         raise e
-        # else:
-        #     print(responses)
-        #     # for response in responses:
-        #     #     print(instance)
+                print("Volume size:", volume_size)
 
 for region in regions:
-    print("Region:", region)
+    print("Region": region)
     list_instances()
