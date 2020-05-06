15ae18e916b96aee06d034ec544ff9b2a95e7ec4
imcuster <imcuster@gmail.com>
2020-05-06 15:03:15 -0400

    Progress notes and cloudwatch troubles

--- python_query/ec2_query.py
+++ python_query/ec2_query.py
@@ -2,21 +2,25 @@
 
 import boto3
 from botocore.exceptions import ClientError
-#from openpxyl import Workbook
+from openpxyl import Workbook
 import argparse
+import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--profile', help='Select the AWS profile you will run this script with.', default='default')
 parser.add_argument('--region', help='Select the region you will run this script against.', default='us-east-1')
+parser.add_argument('--filename', help='Name for destination .xlsx file.', default='ec2_instances.xlsx')
 args = parser.parse_args()
 
 session = boto3.session.Session(profile_name=args.profile)
 regions = session.get_available_regions('ec2')
+workbook = Workbook()
+sheet = workbook.active
 #region_name = args.region
 
 def list_instances():
-    #for region in regions:
     ec2 = session.client(service_name='ec2', region_name=region)
+    cw = session.client(service_name='cloudwatch', region_name=region)
     instances = ec2.describe_instances()
     disk_info = ec2.describe_volumes()
     for instance in instances['Reservations']:
@@ -32,10 +36,35 @@ def list_instances():
             print("Number of attached disks:", len(volume_info))
             # Get information about attached volumes
             for volume in volume_info:
+                # # Attempting to define a usable CloudWatch metric.
+                # disk_metrics = cw.get_metric_data(
+                #     MetricDataQueries=[
+                #         {
+                #             'Id': 'disk_utilization',
+                #             "MetricStat" : {
+                #                 "Metric": {
+                #                     "Namespace": "AWS/EC2",
+                #                     "MetricName": "DiskWriteBytes",
+                #                     "Dimensions": [
+                #                         {
+                #                             'Name': 'InstanceId',
+                #                             'Value': instance
+                #                         },
+                #                     ]
+                #                 },
+                #                 'Period': 86400,
+                #                 'Stat': 'DiskWriteBytes',
+                #                 'Unit': 'Bytes'
+                #             },
+                #         ],
+                #     StartTime=datetime(2020, 5, 1),
+                #     EndTime=datetime(2020, 5, 6)
+                # )
                 volume_id = volume['Ebs']['VolumeId']
                 #print(volume['Ebs']['VolumeId'])
                 print("Volume ID:", volume_id)
                 print("Volume mount point:", volume['DeviceName'])
+                print(disk_metrics)
 
     # for region in regions:
     #     ec2 = session.client(service_name='ec2', region_name=region)
@@ -53,5 +82,6 @@ def list_instances():
         #     #     print(instance)
 
 for region in regions:
+    #TODO: skip empty regions to increase speed / reduce cost
     print("Region:", region)
     list_instances()
