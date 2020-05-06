ca4c0e62b3f1b85c827c62e016b87f4041efd5c1
imcuster <imcuster@gmail.com>
2020-05-06 14:39:22 -0400

    Terraform troubleshooting

--- terraform_infra/modules/ec2/main.tf
+++ terraform_infra/modules/ec2/main.tf
@@ -18,10 +18,10 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ec2_deployment" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
-  key_name                    = aws_key_pair.ec2_deployment.key_name
+  #key_name                    = aws_key_pair.ec2_deployment.key_name
   associate_public_ip_address = false
   #vpc_security_group_ids      = [aws_security_group.lockdown]
-  ebs_optimized               = true
+  #ebs_optimized               = true
 
   tags = {
     Terraform = true
@@ -41,10 +41,10 @@ resource "aws_volume_attachment" "data_attach" {
   volume_id   = aws_ebs_volume.data.id
 }
 
-resource "aws_key_pair" "ec2_deployment" {
-  key_name   = "imcuster_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDzCoT/n0DF2lUmTwjhwGVc8E/useQemvWMVwcRsL67hTDUMsk7C2oeiA3M5UmcYV8Qb8cyBeth/EInPsAEBOklXP08jjQ7UhVgKNc5HnDBgzp5eADoUkJKkMgZKyp3el8SwiRf9HbIS01qcX9cfc8zNTGTiv9mNRO+ENXRch4QVd9HSjg27EqXL3L+TaiTaDUzxkuTJJidqe8W4BMdyX2zBlPLPxxU65SZHtm79Y1bSGqe7btX+Nv1M5/FwzaH0AOK8glAW9Z7qoknbKT5hdzO2tmmc73zQXqnNSrRb2eYvNMXUMATsUS+YQ3s4OjyPut9+hX0EIL2jZGplMN/9sZF Ian@RAIN-KING.local"
-}
+// resource "aws_key_pair" "ec2_deployment" {
+//   key_name   = "imcuster_key"
+//   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDzCoT/n0DF2lUmTwjhwGVc8E/useQemvWMVwcRsL67hTDUMsk7C2oeiA3M5UmcYV8Qb8cyBeth/EInPsAEBOklXP08jjQ7UhVgKNc5HnDBgzp5eADoUkJKkMgZKyp3el8SwiRf9HbIS01qcX9cfc8zNTGTiv9mNRO+ENXRch4QVd9HSjg27EqXL3L+TaiTaDUzxkuTJJidqe8W4BMdyX2zBlPLPxxU65SZHtm79Y1bSGqe7btX+Nv1M5/FwzaH0AOK8glAW9Z7qoknbKT5hdzO2tmmc73zQXqnNSrRb2eYvNMXUMATsUS+YQ3s4OjyPut9+hX0EIL2jZGplMN/9sZF Ian@RAIN-KING.local"
+// }
 
 // resource "aws_security_group" "lockdown" {
 //   name        = "lockdown"
